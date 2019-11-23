bca0f9483028a216996f58b390b60835d902bbec
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2019-11-22 20:44:21 -0500

    oh my god, the asynchronous searchbar actually works this is the greatest day of my life

--- ifyouinvested/src/components/SearchBar.js
+++ ifyouinvested/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import axios from 'axios'
-import Select from 'react-select'
+import AsyncSelect from 'react-select/async'
 import './SearchBar.css'
 let searchTimer;
 class SearchBar extends Component{
@@ -11,35 +11,32 @@ class SearchBar extends Component{
             search_results:[]
         }
     }
-    searchFor=()=>{
-        if(this.state.ticker!==""){
-            let keyword = this.state.ticker;
+    async searchFor(evt){
+        if(evt!==""){
+            let keyword = evt;
             let av_key = "L2JPMX2ZDKA2DFUY";
             let av_search_url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+keyword+"&apikey="+av_key; 
-            axios.get(av_search_url)
-            .then(search_result=>{
-                this.state.search_results =[];
-                let search_length = Object.keys(search_result.data["bestMatches"]).length-1;
-                for(let i = 0; i < search_length; i++){
-                    let current_search = search_result.data["bestMatches"][i];
-                    let ticker = current_search["1. symbol"];
-                    let name = current_search["2. name"];
-                    this.state.search_results.push({value:ticker, label:name+"("+ticker+")"});
-                }
-            })
-            .catch(err=>{
-                console.log(err);
-            });
-        }
-    }
+            let search_result = await axios.get(av_search_url)
+            let search_results =[];
+            let search_length = Object.keys(search_result.data["bestMatches"]).length-1;
+            for(let i = 0; i < search_length; i++){
+                let current_search = search_result.data["bestMatches"][i];
+                let ticker = current_search["1. symbol"];
+                let name = current_search["2. name"];
+                search_results.push({value:ticker, label:name+"("+ticker+")"});
+            }
+            console.log(search_results);
+            return search_results;
+        }    
+    };
     render(){
         return(
             <div>
-                <Select
-                    options={this.state.search_results}
-                    placeholder="Select your stock"
-                    onChange={evt=>this.getResults(evt)}
-                    onInputChange={evt=>this.updateResults(evt)}
+                <AsyncSelect 
+                    cacheOptions 
+                    defaultOptions
+                    onChange ={this.getResults}
+                    loadOptions={this.updateResults}
                 />  
             </div>
         )
@@ -48,14 +45,12 @@ class SearchBar extends Component{
         console.log(evt);
         this.props.sendTicker(evt.value);
     }
-    updateResults = (evt) =>{
-        console.log(evt);
-        clearTimeout(searchTimer);
-        searchTimer = setTimeout(this.searchFor, 2000);
-        this.setState({
-            ticker:evt
+    updateResults = evt =>
+        new Promise(resolve=>{
+            clearTimeout(searchTimer);
+            searchTimer = setTimeout(()=>{
+                resolve(this.searchFor(evt));
+            }, 2000);
         });
-  }
-
 }
 export default SearchBar
